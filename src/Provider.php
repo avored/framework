6902a15e03c7260ec25882e388adc9dd4f1a3c59
Purvesh <ind.purvesh@gmail.com>
2018-03-10 12:56:12 +1300

    Widget Commit

---
+++ src/Provider.php
@@ -0,0 +1,48 @@
+<?php
+namespace AvoRed\Framework;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+
+class Provider extends ServiceProvider
+{
+
+    protected $providers = [
+        \AvoRed\Framework\Widget\Provider::class
+    ];
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerProviders();
+    }
+
+
+    /**
+     * Registering AvoRed E commerce Services
+     * e.g Admin Menu
+     *
+     * @return void
+     */
+    protected function registerProviders()
+    {
+        foreach ($this->providers as $provider) {
+            App::register($provider);
+        }
+    }
+
+}
---
+++ src/Widget/Contracts/Widget.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AvoRed\Framework\Widget\Contracts;
+
+interface Widget
+{
+
+    /**
+     * Get/Set Widget Label
+     * @return string $label
+     */
+    public function label();
+
+
+    /**
+     * Get/Set Widget Type
+     * @return string $type
+     */
+    public function type();
+
+}
