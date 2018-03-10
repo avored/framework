24f8575c9cd9fc4f089b1cd9be11cb68a3df4cd3
Purvesh <ind.purvesh@gmail.com>
2018-03-10 14:49:07 +1300

    move image

---
+++ src/Image/Facade.php
@@ -0,0 +1,12 @@
+<?php
+namespace AvoRed\Framework\Image;
+
+use Illuminate\Support\Facades\Facade as LaravelFacade;
+
+class Facade extends LaravelFacade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'image';
+    }
+}
