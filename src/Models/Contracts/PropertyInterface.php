7eb838e4dc7f25d95fe25ccf4116625dc052f679
Purvesh <ind.purvesh@gmail.com>
2018-08-08 15:40:25 +1200

    some updates in model interface

--- src/Models/Contracts/PropertyInterface.php
+++ src/Models/Contracts/PropertyInterface.php
@@ -11,11 +11,27 @@ interface PropertyInterface
      * @return \AvoRed\Framework\Models\Database\Property
      */
     public function find($id);
+    
+    /**
+     * Find an Property collection by given an array of Ids
+     *
+     * @param array $ids
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function find($ids);
 
     /**
-    * Product Property Query Builder
-    *
-    * @return \Illuminate\Database\Eloquent\Builder
-    */
+     * Product Property Query Builder
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function query();
+
+    /**
+     * Product Property Query Builder
+     * 
+     * @param array $data
+     * @return \AvoRed\Framework\Models\Database\Property
+     */
+    public function create($data);
 }
