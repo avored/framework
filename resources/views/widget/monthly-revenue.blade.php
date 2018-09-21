4b463a0c017b94dec772acedd3a0eb12cc2cca1c
indpurvesh <ind.purvesh@gmail.com>
2018-09-22 11:43:19 +1200

    added an monthly revenue widget

---
+++ resources/views/widget/monthly-revenue.blade.php
@@ -0,0 +1,10 @@
+<div class="widget">
+    <div class="card mb-3 d-block" style="cursor: move;">
+        <div class="card-header">
+            <h5 class="card-title text-center">{{ __('avored-framework::lang.admin-dashboard-monthly-revenue-title') }}</h5>
+        </div>        
+        <div class="card-body">
+            <p class="card-text text-center">{{ $currencySymbol }}{{ $monthlyRevenue }}</p>
+        </div>
+    </div>
+</div>
