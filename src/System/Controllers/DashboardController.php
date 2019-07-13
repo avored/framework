5a8f8d8015a83907bf2763538251659722ae66c2
Purvesh <ind.purvesh@gmail.com>
2019-07-13 17:42:57 +1200

    fixed the test

--- src/System/Controllers/DashboardController.php
+++ src/System/Controllers/DashboardController.php
@@ -1,9 +1,6 @@
 <?php
 namespace AvoRed\Framework\System\Controllers;
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
 class DashboardController
 {
     /**
@@ -12,38 +9,6 @@ class DashboardController
      */
     public function index()
     {
-        Schema::create('attribute_product', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('attribute_id');
-            $table->unsignedBigInteger('product_id');
-
-            $table->foreign('attribute_id')->references('id')->on('attributes');
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->timestamps();
-        });
-
-        Schema::create('attribute_product_values', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('attribute_id');
-            $table->unsignedBigInteger('product_id');
-            $table->unsignedBigInteger('attribute_dropdown_option_id');
-
-            $table->foreign('attribute_id')->references('id')->on('attributes');
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('attribute_dropdown_option_id')->references('id')->on('attribute_dropdown_options');
-            $table->timestamps();
-        });
-
-        Schema::create('product_variations', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('variation_id');
-            $table->unsignedBigInteger('product_id');
-
-            $table->foreign('variation_id')->references('id')->on('products');
-            $table->foreign('product_id')->references('id')->on('products');
-            $table->timestamps();
-        });
-
         return view('avored::admin');
     }
 }
