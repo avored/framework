303755e45e86b5b31871a2919f8eb17dd61992a9
Purvesh <ind.purvesh@gmail.com>
2019-09-06 11:41:53 +1200

    fetch vue component dynamically to reduce JS file size

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,67 +12,58 @@ if (token) {
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-import VueQuillEditor from 'vue-quill-editor'
-import Vddl from 'vddl'
-
-import Row from 'ant-design-vue/lib/row'
-import Col from 'ant-design-vue/lib/col'
-import Slider from 'ant-design-vue/lib/slider'
+// import Row from 'ant-design-vue/lib/row'
+// import Col from 'ant-design-vue/lib/col'
+// import Slider from 'ant-design-vue/lib/slider'
 import Layout from 'ant-design-vue/lib/layout'
 import Menu from 'ant-design-vue/lib/menu'
-import Icon from 'ant-design-vue/lib/icon'
-import Table from 'ant-design-vue/lib/table'
+// import Icon from 'ant-design-vue/lib/icon'
+// import Table from 'ant-design-vue/lib/table'
 import Form from 'ant-design-vue/lib/form'
-import Input from 'ant-design-vue/lib/input'
+// import Input from 'ant-design-vue/lib/input'
 import Select from 'ant-design-vue/lib/select'
-import Button from 'ant-design-vue/lib/button'
-import Avatar from 'ant-design-vue/lib/avatar'
-import Card from 'ant-design-vue/lib/card'
+// import Button from 'ant-design-vue/lib/button'
+// import Avatar from 'ant-design-vue/lib/avatar'
+// import Card from 'ant-design-vue/lib/card'
 import Breadcrumb from 'ant-design-vue/lib/breadcrumb'
 import Tabs from 'ant-design-vue/lib/tabs'
 import Modal from 'ant-design-vue/lib/modal'
-import Switch from 'ant-design-vue/lib/switch'
+// import Switch from 'ant-design-vue/lib/switch'
 import Notification from 'ant-design-vue/lib/notification'
-import Divider from 'ant-design-vue/lib/divider'
 import Dropdown from 'ant-design-vue/lib/dropdown'
-import Radio from 'ant-design-vue/lib/radio'
-import Upload from 'ant-design-vue/lib/upload'
-import Drawer from 'ant-design-vue/lib/drawer'
-
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(Slider)
 Vue.use(Layout)
 Vue.use(Menu)
-Vue.use(Icon)
-Vue.use(Table)
 Vue.use(Form)
-Vue.use(Input)
+// Vue.use(Input)
 Vue.use(Select)
-Vue.use(Button)
-Vue.use(Avatar)
-Vue.use(Card)
+// Vue.use(Button)
+// Vue.use(Avatar)
+// Vue.use(Card)
 Vue.use(Breadcrumb)
 Vue.use(Tabs)
 Vue.use(Modal)
-Vue.use(Switch)
-Vue.use(Notification)
-Vue.use(Divider)
+// Vue.use(Switch)
+// Vue.use(Notification)
+// Vue.use(Divider)
 Vue.use(Dropdown)
-Vue.use(Radio)
-Vue.use(Upload)
-Vue.use(Drawer)
+// Vue.use(Radio)
+// Vue.use(Upload)
+// Vue.use(Drawer)
 
+
+Vue.component('a-icon', () => import('ant-design-vue/lib/icon'))
+Vue.component('a-avatar', () => import('ant-design-vue/lib/avatar'))
+Vue.component('a-row', () => import('ant-design-vue/lib/row'))
+Vue.component('a-col', () => import('ant-design-vue/lib/col'))
+Vue.component('a-icon', () => import('ant-design-vue/lib/icon'))
+Vue.component('a-drawer', () => import('ant-design-vue/lib/drawer'))
+Vue.component('a-card', () => import('ant-design-vue/lib/card'))
+Vue.component('a-table', () => import('ant-design-vue/lib/table'))
+Vue.component('a-button', () => import('ant-design-vue/lib/button'))
+Vue.component('a-upload', () => import('ant-design-vue/lib/upload'))
+Vue.component('a-upload', () => import('ant-design-vue/lib/upload'))
+Vue.component('a-input', () => import('ant-design-vue/lib/input'))
+Vue.component('a-switch', () => import('ant-design-vue/lib/switch'))
 
 Vue.prototype.$notification = Notification;
 Vue.prototype.$confirm = Modal.confirm;
@@ -82,63 +73,64 @@ Vue.prototype.$error = Modal.error;
 Vue.prototype.$warning = Modal.warning;
 Vue.prototype.$confirm = Modal.confirm;
 
+import Vddl from 'vddl'
 Vue.use(Vddl)
-Vue.use(VueQuillEditor);
 
-Vue.component('order-table', require('../components/order/order/OrderTable.vue').default);
+Vue.component('order-table', () => import('../components/order/order/OrderTable.vue'))
 
-Vue.component('language-table', require('../components/system/language/LanguageTable.vue').default);
-Vue.component('language-save', require('../components/system/language/LanguageSave.vue').default);
+Vue.component('language-table', () => import('../components/system/language/LanguageTable.vue'))
+Vue.component('language-save', () => import('../components/system/language/LanguageSave.vue'))
 
-Vue.component('user-group-table', require('../components/user/user-group/UserGroupTable.vue').default);
-Vue.component('user-group-save', require('../components/user/user-group/UserGroupSave.vue').default);
 
-Vue.component('tax-group-table', require('../components/system/tax-group/TaxGroupTable.vue').default);
-Vue.component('tax-group-save', require('../components/system/tax-group/TaxGroupSave.vue').default);
+Vue.component('user-group-table', () => import('../components/user/user-group/UserGroupTable.vue'))
+Vue.component('user-group-save', () => import('../components/user/user-group/UserGroupSave.vue'))
 
-Vue.component('tax-rate-table', require('../components/system/tax-rate/TaxRateTable.vue').default);
-Vue.component('tax-rate-save', require('../components/system/tax-rate/TaxRateSave.vue').default);
+Vue.component('tax-group-table', () => import('../components/system/tax-group/TaxGroupTable.vue'))
+Vue.component('tax-group-save', () => import('../components/system/tax-group/TaxGroupSave.vue'))
 
-Vue.component('attribute-table', require('../components/catalog/attribute/AttributeTable.vue').default);
-Vue.component('attribute-save', require('../components/catalog/attribute/AttributeSave.vue').default);
+Vue.component('tax-rate-table', () => import('../components/system/tax-rate/TaxRateTable.vue'))
+Vue.component('tax-rate-save', () => import('../components/system/tax-rate/TaxRateSave.vue'))
 
-Vue.component('property-table', require('../components/catalog/property/PropertyTable.vue').default);
-Vue.component('property-save', require('../components/catalog/property/PropertySave.vue').default);
+Vue.component('attribute-table', () => import('../components/catalog/attribute/AttributeTable.vue'))
+Vue.component('attribute-save', () => import('../components/catalog/attribute/AttributeSave.vue'))
 
-Vue.component('product-table', require('../components/catalog/product/ProductIndex.vue').default);
-Vue.component('product-save', require('../components/catalog/product/ProductSave.vue').default);
+Vue.component('property-table', () => import('../components/catalog/property/PropertyTable.vue'))
+Vue.component('property-save', () => import('../components/catalog/property/PropertySave.vue'))
 
-Vue.component('state-table', require('../components/system/state/StateTable.vue').default);
-Vue.component('state-save', require('../components/system/state/StateSave.vue').default);
+Vue.component('product-table', () => import('../components/catalog/product/ProductIndex.vue'))
+Vue.component('product-save', () => import('../components/catalog/product/ProductSave.vue'))
 
-Vue.component('currency-table', require('../components/system/currency/CurrencyIndex.vue').default);
-Vue.component('currency-save', require('../components/system/currency/CurrencySave.vue').default);
+Vue.component('state-table', () => import('../components/system/state/StateTable.vue'))
+Vue.component('state-save', () => import('../components/system/state/StateSave.vue'))
 
-Vue.component('category-table', require('../components/catalog/category/CategoryTable.vue').default);
-Vue.component('category-save', require('../components/catalog/category/CategorySave.vue').default);
+Vue.component('currency-table', () => import('../components/system/currency/CurrencyIndex.vue'))
+Vue.component('currency-save', () => import('../components/system/currency/CurrencySave.vue'))
 
-Vue.component('configuration-save', require('../components/system/configuration/ConfigurationSave.vue').default);
+Vue.component('category-table', () => import('../components/catalog/category/CategoryTable.vue'))
+Vue.component('category-save', () => import('../components/catalog/category/CategorySave.vue'))
 
-Vue.component('menu-save', require('../components/cms/menu/MenuSave.vue').default);
-Vue.component('menu-table', require('../components/cms/menu/MenuTable.vue').default);
+Vue.component('configuration-save', () => import('../components/system/configuration/ConfigurationSave.vue'))
 
-Vue.component('page-table', require('../components/cms/page/PageTable.vue').default);
-Vue.component('page-save', require('../components/cms/page/PageSave.vue').default);
+Vue.component('menu-save', () => import('../components/cms/menu/MenuSave.vue'))
+Vue.component('menu-table', () => import('../components/cms/menu/MenuTable.vue'))
 
-Vue.component('order-status-table', require('../components/order/order-status/OrderStatusTable.vue').default);
-Vue.component('order-status-save', require('../components/order/order-status/OrderStatusSave.vue').default);
+Vue.component('page-table', () => import('../components/cms/page/PageTable.vue'))
+Vue.component('page-save', () => import('../components/cms/page/PageSave.vue'))
 
-Vue.component('role-index', require('../components/system/role/RoleTable.vue').default);
-Vue.component('system-role-save', require('../components/system/role/RoleSave.vue').default);
+Vue.component('order-status-table', () => import('../components/order/order-status/OrderStatusTable.vue'))
+Vue.component('order-status-save', () => import('../components/order/order-status/OrderStatusSave.vue'))
 
-Vue.component('admin-user-table', require('../components/system/admin-user/AdminUserTable.vue').default);
-Vue.component('admin-user-save', require('../components/system/admin-user/AdminUserSave.vue').default);
+Vue.component('role-index', () => import('../components/system/role/RoleTable.vue'))
+Vue.component('system-role-save', () => import('../components/system/role/RoleSave.vue'))
 
-Vue.component('avored-layout', require('../components/system/Layout.vue').default);
-Vue.component('avored-flash', require('../components/system/Flash.vue').default);
-Vue.component('login-fields', require('../components/system/LoginFields.vue').default);
-Vue.component('password-reset-page', require('../components/system/PasswordResetPage.vue').default);
-Vue.component('password-new-page', require('../components/system/PasswordNewPage.vue').default);
+Vue.component('admin-user-table', () => import('../components/system/admin-user/AdminUserTable.vue'))
+Vue.component('admin-user-save', () => import('../components/system/admin-user/AdminUserSave.vue'))
+
+Vue.component('avored-layout', () => import('../components/system/Layout.vue'))
+Vue.component('avored-flash', () => import('../components/system/Flash.vue'))
+Vue.component('login-fields', () => import('../components/system/LoginFields.vue'))
+Vue.component('password-reset-page', () => import('../components/system/PasswordResetPage.vue'))
+Vue.component('password-new-page', () => import('../components/system/PasswordNewPage.vue'))
 
 
 /**
