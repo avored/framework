445b75ea87b0423bed2b8783c5e5c572e7c659ad
Purvesh <ind.purvesh@gmail.com>
2020-03-29 10:22:09 +1000

    migrate login to tailwindcss

---
+++ resources/js/tailwind.js
@@ -0,0 +1,139 @@
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+import AvoRed from './avored'
+//import router from './router'
+//import apolloProvider from './vue-apollo'
+
+window.Vue = require('vue')
+window.AvoRed = AvoRed
+
+//import i18n from 'vue-i18n'
+
+
+// import Layout from 'ant-design-vue/lib/layout'
+// import Menu from 'ant-design-vue/lib/menu'
+// import Form from 'ant-design-vue/lib/form'
+// import Select from 'ant-design-vue/lib/select'
+// import Breadcrumb from 'ant-design-vue/lib/breadcrumb'
+// import Tabs from 'ant-design-vue/lib/tabs'
+// import Modal from 'ant-design-vue/lib/modal'
+// import Notification from 'ant-design-vue/lib/notification'
+// import Dropdown from 'ant-design-vue/lib/dropdown'
+
+// Vue.use(Layout)
+// Vue.use(Menu)
+// Vue.use(Form)
+// Vue.use(Select)
+// Vue.use(Breadcrumb)
+// Vue.use(Tabs)
+// Vue.use(Modal)
+// Vue.use(Dropdown)
+
+
+// Vue.component('a-date-picker', () => import('ant-design-vue/lib/date-picker'))
+// Vue.component('a-icon', () => import('ant-design-vue/lib/icon'))
+// Vue.component('a-avatar', () => import('ant-design-vue/lib/avatar'))
+// Vue.component('a-row', () => import('ant-design-vue/lib/row'))
+// Vue.component('a-col', () => import('ant-design-vue/lib/col'))
+// Vue.component('a-icon', () => import('ant-design-vue/lib/icon'))
+// Vue.component('a-drawer', () => import('ant-design-vue/lib/drawer'))
+// Vue.component('a-card', () => import('ant-design-vue/lib/card'))
+// Vue.component('a-table', () => import('ant-design-vue/lib/table'))
+// Vue.component('a-button', () => import('ant-design-vue/lib/button'))
+// Vue.component('a-upload', () => import('ant-design-vue/lib/upload'))
+// Vue.component('a-upload', () => import('ant-design-vue/lib/upload'))
+// Vue.component('a-input', () => import('ant-design-vue/lib/input'))
+// Vue.component('a-switch', () => import('ant-design-vue/lib/switch'))
+// Vue.component('a-tag', () => import('ant-design-vue/lib/tag'))
+
+// Vue.prototype.$notification = Notification;
+// Vue.prototype.$confirm = Modal.confirm;
+// Vue.prototype.$info = Modal.info;
+// Vue.prototype.$success = Modal.success;
+// Vue.prototype.$error = Modal.error;
+// Vue.prototype.$warning = Modal.warning;
+// Vue.prototype.$confirm = Modal.confirm;
+
+import Vddl from 'vddl'
+Vue.use(Vddl)
+
+// Vue.component('order-table', () => import('../components/order/order/OrderTable.vue'))
+
+// Vue.component('language-table', () => import('../components/system/language/LanguageTable.vue'))
+// Vue.component('language-save', () => import('../components/system/language/LanguageSave.vue'))
+
+
+// Vue.component('user-group-table', () => import('../components/user/user-group/UserGroupTable.vue'))
+// Vue.component('user-group-save', () => import('../components/user/user-group/UserGroupSave.vue'))
+
+// Vue.component('tax-group-table', () => import('../components/system/tax-group/TaxGroupTable.vue'))
+// Vue.component('tax-group-save', () => import('../components/system/tax-group/TaxGroupSave.vue'))
+
+// Vue.component('tax-rate-table', () => import('../components/system/tax-rate/TaxRateTable.vue'))
+// Vue.component('tax-rate-save', () => import('../components/system/tax-rate/TaxRateSave.vue'))
+
+// Vue.component('attribute-table', () => import('../components/catalog/attribute/AttributeTable.vue'))
+// Vue.component('attribute-save', () => import('../components/catalog/attribute/AttributeSave.vue'))
+
+// Vue.component('property-table', () => import('../components/catalog/property/PropertyTable.vue'))
+// Vue.component('property-save', () => import('../components/catalog/property/PropertySave.vue'))
+
+// Vue.component('product-table', () => import('../components/catalog/product/ProductIndex.vue'))
+// Vue.component('product-save', () => import('../components/catalog/product/ProductSave.vue'))
+
+// Vue.component('state-table', () => import('../components/system/state/StateTable.vue'))
+// Vue.component('state-save', () => import('../components/system/state/StateSave.vue'))
+
+// Vue.component('currency-table', () => import('../components/system/currency/CurrencyIndex.vue'))
+// Vue.component('currency-save', () => import('../components/system/currency/CurrencySave.vue'))
+
+// Vue.component('category-table', () => import('../components/catalog/category/CategoryTable.vue'))
+// Vue.component('category-save', () => import('../components/catalog/category/CategorySave.vue'))
+
+// Vue.component('configuration-save', () => import('../components/system/configuration/ConfigurationSave.vue'))
+
+// Vue.component('menu-save', () => import('../components/cms/menu/MenuSave.vue'))
+// Vue.component('menu-table', () => import('../components/cms/menu/MenuTable.vue'))
+
+// Vue.component('page-table', () => import('../components/cms/page/PageTable.vue'))
+// Vue.component('page-save', require('../components/cms/page/PageSave.vue').default)
+
+// Vue.component('order-status-table', () => import('../components/order/order-status/OrderStatusTable.vue'))
+// Vue.component('order-status-save', () => import('../components/order/order-status/OrderStatusSave.vue'))
+
+// Vue.component('role-index', () => import('../components/system/role/RoleTable.vue'))
+// Vue.component('system-role-save', () => import('../components/system/role/RoleSave.vue'))
+
+// Vue.component('admin-user-table', () => import('../components/system/admin-user/AdminUserTable.vue'))
+// Vue.component('admin-user-save', require('../components/system/admin-user/AdminUserSave.vue').default)
+
+// Vue.component('promotion-code-table', () => import('../components/promotion/promotion-code/PromotionCodeTable.vue'))
+// Vue.component('promotion-code-edit', () => import('../components/promotion/promotion-code/PromotionCodeEdit.vue'))
+
+// Vue.component('avored-layout', () => import('../components/system/Layout.vue'))
+// Vue.component('avored-flash', () => import('../components/system/Flash.vue'))
+Vue.component('login-fields', require('../components/system/LoginFields.vue').default)
+// Vue.component('password-reset-page', () => import('../components/system/PasswordResetPage.vue'))
+// Vue.component('password-new-page', () => import('../components/system/PasswordNewPage.vue'))
+
+
+
+
+const app = new Vue({
+    el: '#app',
+    //router,
+    //i18n,
+    //apolloProvider
+});
+
+export const EventBus = new Vue();
--- resources/views/system/login/form.blade.php
+++ resources/views/system/login/form.blade.php
@@ -9,86 +9,92 @@
 
     <title>@yield('meta_title', 'AvoRed E commerce')</title>
 
-    <script defer src="{{ asset('avored-admin/js/app.js') }}"></script>
+    <script defer src="{{ asset('avored-admin/js/tailwind.js') }}"></script>
     
     <!-- Styles -->
-    <link href="{{ asset('avored-admin/css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('avored-admin/css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('avored-admin/css/tailwind.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <login-fields inline-template>
-              <div>
-                <a-row type="flex" align="middle">
-                    <a-col :span="12">
-                        <a-row type="flex">
-                        <a-col :span="20" :offset="2">
-                            <a-card title="Card title">
-                                <a-form
-                                    :form="loginForm"
-                                    method="post"
-                                    action=""
-                                    @submit="handleSubmit"
-                                >
-                                    @csrf
-                                    <a-form-item label="Email Address">
-                                    <a-input
-                                        :auto-focus="true"
-                                        name="email"
-                                        v-decorator="[
-                                        'email',
-                                        {
-                                            rules: [
-                                                {   required: true, 
-                                                    message: 'Email Address is required' 
-                                                }
-                                            ]
-                                        }
-                                        ]"
-                                    />
-                                    </a-form-item>
-                                    
-                                    <a-form-item 
-                                    
-                                        label="Password Label">
-                                        <a-input
-                                            name="password"
-                                            type="password"
-                                            v-decorator="[
-                                            'password',
-                                            {rules: [{ required: true, message: 'Password is required' }]}
-                                            ]"
-                                        />
-                                    </a-form-item>
-                                    
-                                    <a-form-item>
-                                        <a-button
-                                            type="primary"
-                                            :loading="loadingSubmitBtn"
-                                            html-type="submit"
-                                        >
-                                            Login Button
-                                        </a-button>
+            <div>
+                <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-md w-full">
+                    <div>
+                        <a href="https://avored.com" target="_blank">
+                            <img class="mx-auto h-12 w-auto" 
+                                src="{{ asset('/images/logo.svg') }}" 
+                                alt="AvoRed Ecommerce" />
+                        </a>
+                        <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+                            Sign in to admin account
+                        </h2>
+                        <p class="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
+                        </p>
+                    </div>
+                    <form class="mt-8" action="{{ route('admin.login.post') }}" method="POST">
+                        @csrf()
+                        <input type="hidden" name="remember" value="true" />
+                        <div class="rounded-md shadow-sm">
+                            <div class="mt-3">
+                                <input aria-label="Email address" 
+                                    name="email" 
+                                    autofocus
+                                    type="email" required 
+                                    class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-red focus:border-red-300 focus:z-10 sm:text-sm sm:leading-5" 
+                                    placeholder="Email address" />
+                            </div>
 
-                                        <a class="ml-1" href="#forgot-password-url">Forgot password?</a>
-                                    </a-form-item>
-                                </a-form>
-                            </a-card>
-                        </a-col>
-                        </a-row>
-                    </a-col>
-                
-                    <a-col :span="12">
-                    <a-row type="flex" align="middle" class="h-100 text-center">
-                    <a-col :span="24">
-                            <img 
-                                class="height-100"
-                                src="/avored-admin/images/avored_admin_login.svg" 
-                                width="55%" alt="AvoRed Admin Login" />
-                        </a-col>
-                        </a-row>
-                    </a-col>
-                </a-row>
-            </div>
+                            <div class="mt-3">
+                                <input aria-label="Password" 
+                                    name="password" 
+                                    type="password" 
+                                    required 
+                                    class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-red  focus:border-red-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="Password" />
+                            </div>
+                        </div>
+
+                        <div class="mt-6 flex items-center justify-between">
+                            <div class="flex items-center">
+                                <input id="remember_me" type="checkbox" 
+                                    class="form-checkbox h-4 w-4 text-red-600 transition duration-150 ease-in-out" 
+                                />
+                                <label for="remember_me" class="ml-2 block text-sm leading-5 text-gray-900">
+                                    Remember me
+                                </label>
+                            </div>
+
+                            <div class="text-sm leading-5">
+                                <a href="#" class="font-medium text-red-600 hover:text-red-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                                    Forgot your password?
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="mt-6">
+                            <button 
+                                type="submit" 
+                                class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-700 transition duration-150 ease-in-out"
+                            >
+                            <span class="absolute left-0 inset-y pl-3">
+                                <svg 
+                                    class="h-5 w-5 text-red-500 group-hover:text-red-400 transition ease-in-out duration-150" 
+                                    fill="currentColor" 
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" 
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                            </span>
+                                {{ __('Sign In') }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+        </div>
         </login-fields>
     </div>
     @stack('scripts')
