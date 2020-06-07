81d1ba072fb0f8f172cb04ba32e8684395c19fcc
Purvesh <ind.purvesh@gmail.com>
2020-06-07 13:32:18 +1000

    added a menu builder with destroy feature

--- resources/views/cms/menu/_fields.blade.php
+++ resources/views/cms/menu/_fields.blade.php
@@ -72,7 +72,7 @@
                             class="menu-item"
                             :index="index"
                             :wrapper="menus"
-                            :key="'menu-' + menu.id"
+                            :key="index"
                             v-for="(menu, index) in menus">
                         
                             @{{menu.name}}
@@ -89,11 +89,19 @@
                                         :wrapper="submenu.submenus"
                                         :key="'submenu-' + submenu.id"
                                         v-for="(submenu, index) in menu.submenus">
-
                                         @{{ submenu.name }}
-
+                                        <button type="button" @click.prevent="deleteOnClick(menu, true)">
+                                            <svg class="h-6 w-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                <path class="heroicon-ui" d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 012 2v2h5a1 1 0 010 2h-1v12a2 2 0 01-2 2H6a2 2 0 01-2-2V8H3a1 1 0 110-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 011 1v6a1 1 0 01-2 0v-6a1 1 0 011-1zm4 0a1 1 0 011 1v6a1 1 0 01-2 0v-6a1 1 0 011-1z"/>
+                                            </svg>
+                                        </button>
                                     </vddl-draggable>
                             </vddl-list>
+                            <button type="button" @click.prevent="deleteOnClick(menu)">
+                                <svg class="h-6 w-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path class="heroicon-ui" d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 012 2v2h5a1 1 0 010 2h-1v12a2 2 0 01-2 2H6a2 2 0 01-2-2V8H3a1 1 0 110-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 011 1v6a1 1 0 01-2 0v-6a1 1 0 011-1zm4 0a1 1 0 011 1v6a1 1 0 01-2 0v-6a1 1 0 011-1z"/>
+                                </svg>
+                            </button>
                         </vddl-draggable>
                     </vddl-list>
                 </div>
