5b93c297053cb80eadb836aecfb289813d9be159
Purvesh <purvesh.patel@digistorm.com>
2020-06-09 16:09:43 +1000

    updated workflow

---
+++ resources/components/system/forms/AvoRedButtton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "avored-button",
+  props: {
+    buttonText: { type: [String], default: "" },
+    htmlType: { type: [String], default: "button" },
+    type: { type: [String], default: "default" },
+  },
+  data() {
+    return {
+      
+    };
+  },
+  watch: {
+    
+  },
+  mounted() {
+    
+  }
+};
+</script>
