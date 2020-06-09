6ead65c3a85294aa81b379eadff6f30202a394ad
Purvesh <purvesh.patel@digistorm.com>
2020-06-09 16:30:29 +1000

    updated controller

--- src/System/Controllers/ResetPasswordController.php
+++ src/System/Controllers/ResetPasswordController.php
@@ -3,7 +3,7 @@
 namespace AvoRed\Framework\System\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Foundation\Auth\ResetsPasswords;
