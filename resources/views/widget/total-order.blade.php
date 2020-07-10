5787f3355bb0d630ef2830bb7c75302ac9795f38
Purvesh <ind.purvesh@gmail.com>
2020-07-11 08:34:50 +1000

    wip

--- resources/views/widget/total-order.blade.php
+++ resources/views/widget/total-order.blade.php
@@ -1,5 +1,18 @@
-<a-col :span="4" :xs="24" :sm="12" :md="6">
-    <a-card title="{{ __('avored::system.total-order') }}" class="dashboard-widget mt-1 primary">
-        <p class="amount">{{ $value }}</p>
-    </a-card>
-</a-col>
+<div class="w-64 cursor-pointer bg-purple-500 ml-3 border text-center b-gray-400 rounded items-center p-6 text-white">
+    <div class="text-md border-b pb-3 font-bold w-full">
+        <span class="uppercase">
+            {{ __('avored::system.total-order') }}
+        </span>
+    </div>
+    <div class="flex mt-5 text-center justify-around items-center">
+        <div class="w-12 h-12">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M18 6V4H2v2h16zm0 4H2v6h16v-6zM0 4c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm4 8h4v2H4v-2z"/>
+            </svg>
+           
+        </div>
+        <div class="text-4xl ml-5 font-bold">
+            {{ $value }}   
+        </div>
+    </div>
+</div>
--- resources/views/widget/total-revenue.blade.php
+++ resources/views/widget/total-revenue.blade.php
@@ -1,5 +1,17 @@
-<a-col :span="4" :xs="24" :sm="12" :md="6">
-    <a-card title="{{ __('avored::system.total-revenue') }}" class="dashboard-widget mt-1 info">
-        <p class="amount">{{ session()->get('default_currency')->symbol }}{{ $value }}</p>
-    </a-card>
-</a-col>
+<div class="w-64 cursor-pointer bg-blue-600 ml-3 border text-center b-gray-400 rounded items-center p-6 text-white">
+    <div class="text-md border-b pb-3 font-bold w-full">
+        <span class="uppercase">
+            {{ __('avored::system.total-revenue') }}
+        </span>
+    </div>
+    <div class="flex mt-5 text-center justify-around items-center">
+        <div class="w-12 h-12">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M4.13 12H4a2 2 0 1 0 1.8 1.11L7.86 10a2.03 2.03 0 0 0 .65-.07l1.55 1.55a2 2 0 1 0 3.72-.37L15.87 8H16a2 2 0 1 0-1.8-1.11L12.14 10a2.03 2.03 0 0 0-.65.07L9.93 8.52a2 2 0 1 0-3.72.37L4.13 12zM0 4c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4z"/>
+            </svg>
+        </div>
+        <div class="text-4xl ml-5 font-bold">
+            {{ $value }}   
+        </div>
+    </div>
+</div>
