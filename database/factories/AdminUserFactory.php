bca4b12a099a6d4883082453843463d77b59e8d4
JunaidJaved8461 <72268940+JunaidJaved8461@users.noreply.github.com>
2020-10-16 13:19:34 +0500

    Update AdminUserFactory.php

--- database/factories/AdminUserFactory.php
+++ database/factories/AdminUserFactory.php
@@ -1,5 +1,5 @@
 <?php
-namespace AvoRed\Database\Factories;
+namespace AvoRed\Framework\Database\Factories;
 use Faker\Generator as Faker;
 use AvoRed\Framework\Database\Models\Role;
 use AvoRed\Framework\Database\Models\AdminUser;
