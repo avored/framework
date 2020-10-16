be9b783b885625b1d31b740745b45eff30d01ea5
JunaidJaved8461 <72268940+JunaidJaved8461@users.noreply.github.com>
2020-10-16 15:39:38 +0500

    Update PropertyFactory.php

--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -1,6 +1,5 @@
 <?php
-namespace Database\Factories;
-use Illuminate\Database\Eloquent\Factories\Factory;
+
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 use AvoRed\Framework\Database\Models\Property;
