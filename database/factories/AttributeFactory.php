af52d3e0c4524669d3b89d0030f347256d23a2f8
JunaidJaved8461 <72268940+JunaidJaved8461@users.noreply.github.com>
2020-10-16 12:50:26 +0500

    Update AttributeFactory.php

--- database/factories/AttributeFactory.php
+++ database/factories/AttributeFactory.php
@@ -1,5 +1,5 @@
 <?php
-namespace Database\Factories;
+namespace AvoRed\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
