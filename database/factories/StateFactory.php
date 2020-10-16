c01decea143920ecfb0dd9ca215d8fc6f93e0573
JunaidJaved8461 <72268940+JunaidJaved8461@users.noreply.github.com>
2020-10-16 15:23:37 +0500

    Update StateFactory.php

--- database/factories/StateFactory.php
+++ database/factories/StateFactory.php
@@ -1,5 +1,5 @@
 <?php
-namespace AvoRed\Framework\Database\Factories;
+namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Generator as Faker;
 use AvoRed\Framework\Database\Models\State;
