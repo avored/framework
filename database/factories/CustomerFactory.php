e132a4fcce42f8dc3d0d106f31f5a6bea8b92979
JunaidJaved8461 <72268940+JunaidJaved8461@users.noreply.github.com>
2020-10-16 12:48:13 +0500

    Update CustomerFactory.php

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -1,5 +1,6 @@
 <?php
-
+namespace Database\Factories;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Generator as Faker;
 use AvoRed\Framework\Database\Models\Customer;
 use Carbon\Carbon;
