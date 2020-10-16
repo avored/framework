70e57d1709bad68ab395e4b9d0ab7a2bd3e8b157
JunaidJaved8461 <72268940+JunaidJaved8461@users.noreply.github.com>
2020-10-16 15:24:05 +0500

    Update ProductFactory.php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,5 +1,5 @@
 <?php
-namespace AvoRed\Framework\Database\Factories;
+namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
