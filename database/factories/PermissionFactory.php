87ab7c9259db182e5f79f6e9b8760e823b8f6c1f
JunaidJaved8461 <72268940+JunaidJaved8461@users.noreply.github.com>
2020-10-16 15:08:51 +0500

    Update PermissionFactory.php

--- database/factories/PermissionFactory.php
+++ database/factories/PermissionFactory.php
@@ -1,5 +1,5 @@
 <?php
-namespace AvoRed\Framework0\Database\Factories;
+namespace AvoRed\Framework\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Generator as Faker;
 use AvoRed\Framework\Database\Models\Permission;
