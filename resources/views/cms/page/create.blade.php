0eedcb28d5f7e8494bf4b2c2cab008d20b9b3da8
Alexey Abramov <dev.abramoff@gmail.com>
2021-04-07 20:54:27 +0300

    - Removed submit handler for page creation form;

--- resources/views/cms/page/create.blade.php
+++ resources/views/cms/page/create.blade.php
@@ -16,10 +16,9 @@
 <div class="flex items-center">
     <page-save base-url="{{ asset(config('avored.admin_url')) }}" inline-template>
         <div class="block w-full">
-            <form 
+            <form
                 method="post"
-                action="{{ route('admin.page.store') }}"                    
-                @submit="handleSubmit"
+                action="{{ route('admin.page.store') }}"
             >
                 @csrf
                 <avored-tabs>
@@ -32,17 +31,17 @@
                         </avored-tab>
                     @endforeach
                 </avored-tabs>
-                
+
                 <div class="mt-3 py-3">
                     <button type="submit"
                         class="px-6 py-3 font-semibold leading-7  text-white hover:text-white bg-red-600 rounded hover:bg-red-700"
-                    >   
+                    >
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 inline-flex w-4" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M0 2C0 .9.9 0 2 0h14l4 4v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5 0v6h10V2H5zm6 1h3v4h-3V3z"/>
                         </svg>
                         <span class="ml-3">{{ __('avored::system.btn.save') }}</span>
                     </button>
-                    
+
                     <a href="{{ route('admin.page.index') }}"
                         class="px-6 py-3 font-semibold inline-block text-white leading-7 hover:text-white bg-gray-500 rounded hover:bg-gray-600">
                         <span class="leading-7">
