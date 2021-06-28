9449935f35387126b6ce34d559a395bb1c0cfc98
Purvesh <ind.purvesh@gmail.com>
2021-06-28 20:32:12 +1200

    minor changes

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -9,8 +9,8 @@
 
     <title>@yield('meta_title', 'AvoRed E commerce')</title>
     <!-- Styles -->
-    
-    @if(true || env('APP_ENV') === 'local' && file_exists(public_path('mix-manifest.json')))
+
+    @if(env('APP_ENV') === 'local' && file_exists(public_path('mix-manifest.json')))
         <link rel="stylesheet" href="{{ mix('vendor/avored/css/app.css') }}"></link>
     @else
         {!! Asset::renderCSS() !!}
@@ -25,8 +25,8 @@
                     <div class="max-w-md w-full bg-white rounded-md shadow-md p-6">
                         <div>
                             <a href="https://avored.com" target="_blank">
-                                <img class="mx-auto h-12 w-auto" 
-                                    src="{{ asset('/vendor/avored/images/logo_only.svg') }}" 
+                                <img class="mx-auto h-12 w-auto"
+                                    src="{{ asset('/vendor/avored/images/logo_only.svg') }}"
                                     alt="AvoRed Ecommerce" />
                             </a>
                             <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
@@ -48,7 +48,7 @@
                                         input-type="email"
                                         error-text="{{ $errors->first('email') }}"
                                     ></avored-input>
-                                    
+
                                 </div>
 
                                 <div class="mt-3">
@@ -60,14 +60,14 @@
                                         input-type="password"
                                         error-text="{{ $errors->first('password') }}"
                                     ></avored-input>
-                                    
+
                                 </div>
                             </div>
 
                             <div class="mt-6 flex items-center justify-between">
                                 <div class="flex items-center">
-                                    <input id="remember_me" type="checkbox" 
-                                        class="form-checkbox h-4 w-4 text-red-600 transition duration-150 ease-in-out" 
+                                    <input id="remember_me" type="checkbox"
+                                        class="form-checkbox h-4 w-4 text-red-600 transition duration-150 ease-in-out"
                                     />
                                     <label for="remember_me" class="ml-2 block text-sm leading-5 text-gray-900">
                                         {{ __('avored::system.auth.form.remember-me') }}
@@ -75,7 +75,7 @@
                                 </div>
 
                                 <div class="text-sm leading-5">
-                                    <a href="{{ route('admin.password.request') }}" 
+                                    <a href="{{ route('admin.password.request') }}"
                                         class="font-medium text-red-600 hover:text-red-500 focus:outline-none focus:underline">
                                         {{ __('avored::system.auth.form.forgot-password') }}
                                     </a>
@@ -83,17 +83,17 @@
                             </div>
 
                             <div class="mt-6">
-                                <button 
-                                    type="submit" 
+                                <button
+                                    type="submit"
                                     class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-700"
                                 >
                                 <span class="absolute left-0 inset-y pl-3">
-                                    <svg 
-                                        class="h-5 w-5 text-red-500 group-hover:text-red-400" 
-                                        fill="currentColor" 
+                                    <svg
+                                        class="h-5 w-5 text-red-500 group-hover:text-red-400"
+                                        fill="currentColor"
                                         viewBox="0 0 20 20"
                                     >
-                                        <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" 
+                                        <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
                                             clip-rule="evenodd"
                                         />
                                     </svg>
@@ -107,7 +107,7 @@
             </div>
         </login-fields>
     </div>
-    @if(true || env('APP_ENV') === 'local' && file_exists(public_path('mix-manifest.json')))
+    @if(env('APP_ENV') === 'local' && file_exists(public_path('mix-manifest.json')))
         <script src="{{ mix('vendor/avored/js/avored.js') }}"></script>
         @stack('scripts')
         <script src="{{ mix('vendor/avored/js/app.js') }}"></script>
