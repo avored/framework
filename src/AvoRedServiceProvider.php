ab1d241b91cb6f8eb4f206c642016135898d4dc2
indpurvesh <ind.purvesh@gmail.com>
2022-05-14 10:13:09 +1200

    fresh start

---
+++ src/AvoRedServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+namespace AvoRed\Framework;
+
+use Illuminate\Support\ServiceProvider;
+
+class AvoRedServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
