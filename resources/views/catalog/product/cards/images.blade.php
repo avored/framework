016237abce4c5a6d690aca5da16374a59193dfa7
Purvesh <ind.purvesh@gmail.com>
2022-10-16 11:14:54 +1300

    minor changes

---
+++ resources/views/catalog/product/cards/images.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <label
+        class="flex bg-white justify-center w-full h-32 p-4 transition border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+        <span class="flex items-center space-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+            </svg>
+            <span class="font-medium text-gray-600">
+                Drop files to Attach, or
+                <span class="text-blue-600 underline">browse</span>
+            </span>
+        </span>
+        <input type="file" name="file_upload" class="hidden">
+    </label>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     purge: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [],
